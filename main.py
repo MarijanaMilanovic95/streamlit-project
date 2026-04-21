148fb1c0e62ab67b65232126e6f874d117921e47
MarijanaMilanovic95 <milanovic_marijana@hotmail.com>
2026-04-22 00:12:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import streamlit as st
+import app1
+import app2
 
 st.set_page_config(page_title="My Projects", layout="wide")
 
 st.title("📊 My Projects")
 
-import app1
-import app2
-
 page = st.sidebar.selectbox(
     "Choose project",
-    [
-        "Home",
-        "Monte Carlo Simulation",
-        "Optimal Strategy for Take-the-Offer Bonus"
-    ]
+    ["Home", "Monte Carlo Simulation", "Take the Offer Bonus"]
 )
 
 if page == "Home":
-    st.write("Welcome! Choose a project from the sidebar.")
+    st.write("Welcome!")
+
+elif page == "Monte Carlo Simulation":
+    app2.run()
+
+elif page == "Take the Offer Bonus":
+    app1.run()
